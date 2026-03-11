3a2cd2347a4854eb57a6415aff9080398520ed97
AbhimanyuRajoriya <abhimanyusaini294@gmail.com>
2026-03-11 12:19:18 +0530

    config

--- js/config.js
+++ js/config.js
@@ -1,1 +1,4 @@
-d3stxddqh5awki.cloudfront.net
+window.API_BASE =
+  window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost"
+    ? "http://127.0.0.1:8000"
+    : "https://d3stxddqh5awki.cloudfront.net";
