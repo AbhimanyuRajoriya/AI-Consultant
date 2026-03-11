c929ec2e359c4ed28e722064c5a509c979e43362
AbhimanyuRajoriya <abhimanyusaini294@gmail.com>
2026-03-11 11:42:28 +0530

    update auth

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 window.API_BASE =
   window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost"
     ? "http://127.0.0.1:8000"
-    : "http://ec2-32-192-188-153.compute-1.amazonaws.com:8000";
+    : "https://d3stxddqh5awki.cloudfront.net";
