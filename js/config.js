8f34728456924ae1eaebd6602d4605025c44f435
AbhimanyuRajoriya <abhimanyusaini294@gmail.com>
2026-03-11 10:08:27 +0530

    update workflow

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 window.API_BASE = window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost"
   ? "http://127.0.0.1:8000"
-  : "http://3.235.76.111:8000";
+  : "http://32.192.188.153:8000";
